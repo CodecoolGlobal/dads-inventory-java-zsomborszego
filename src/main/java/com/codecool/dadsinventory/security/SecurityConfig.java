43baa8aba828f135044afe9a8a962872e59964cb
zsomborszego <szego.zsombor@gmail.com>
2022-02-16 17:23:53 +0100

    add: secure index enpoint, user can login

--- src/main/java/com/codecool/dadsinventory/security/SecurityConfig.java
+++ src/main/java/com/codecool/dadsinventory/security/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.codecool.dadsinventory.security;
 
+import com.codecool.dadsinventory.security.filter.CustomAuthenticationFilter;
+import com.codecool.dadsinventory.security.filter.CustomAuthorizationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
@@ -8,8 +10,13 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.POST;
 
 @Configurable
 @EnableWebSecurity
@@ -26,7 +33,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+        CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManagerBean());
+        http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.authorizeHttpRequests().antMatchers(POST, "/login/**").permitAll();
+        http.authorizeHttpRequests().antMatchers(GET, "/").permitAll();
+        http.authorizeHttpRequests().antMatchers("/index/**").hasAnyAuthority("ROLE_USER");
+        http.authorizeHttpRequests().anyRequest().authenticated();
+        http.addFilter(customAuthenticationFilter);
+        http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
+
     }
 
 
