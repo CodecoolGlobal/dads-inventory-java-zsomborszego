4ca1d4300f0717d1b4736f27b432d3dbed735b57
zsomborszego <szego.zsombor@gmail.com>
2022-02-16 14:15:23 +0100

    add: basic login for main endpoint

--- src/main/java/com/codecool/dadsinventory/controller/HomeController.java
+++ src/main/java/com/codecool/dadsinventory/controller/HomeController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -40,6 +41,10 @@ public class HomeController {
         String title = "Privacy Policy";
         model.addAttribute("title", title);
         return "privacy";
+    }
+    @GetMapping
+    public String loginPage(){
+        return "login";
     }
 
 
