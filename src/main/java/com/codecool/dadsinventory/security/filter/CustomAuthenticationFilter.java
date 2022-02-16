a42b8a54e1ba13b61d1f082e6c345763c246b4f7
zsomborszego <szego.zsombor@gmail.com>
2022-02-16 17:05:24 +0100

    add: CustomAuthenticationFilter for handle Authentication

---
+++ src/main/java/com/codecool/dadsinventory/security/filter/CustomAuthenticationFilter.java
@@ -0,0 +1,7 @@
+package com.codecool.dadsinventory.security.filter;
+
+
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+}
