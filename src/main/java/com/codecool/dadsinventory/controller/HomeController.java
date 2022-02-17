56cf5290659dc55b2ad95aabe7ed967b38fb7439
zsomborszego <szego.zsombor@gmail.com>
2022-02-17 11:19:07 +0100

    add: get user name from bakced

--- src/main/java/com/codecool/dadsinventory/controller/HomeController.java
+++ src/main/java/com/codecool/dadsinventory/controller/HomeController.java
@@ -5,12 +5,21 @@ import com.codecool.dadsinventory.service.ItemService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.URI;
 import java.util.List;
 
 
@@ -27,10 +36,13 @@ public class HomeController {
 
     @GetMapping("/index")
     public String index(Model model, @RequestParam(name="searchTerm", required = false, defaultValue = "") String searchTerm){
+        String name = SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString();
+        System.out.println(name);
         String title = "Dad's Inventory";
         model.addAttribute("title", title);
         log.info("Searchterm is: " + searchTerm);
         List<Item> items = itemService.getAllBySearchTerm(searchTerm);
+        model.addAttribute("name", name);
         model.addAttribute("title", title);
         model.addAttribute("items", items);
         return "index";
